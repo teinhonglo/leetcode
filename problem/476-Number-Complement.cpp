c0bc687a38390a1f15c05e287f043a6488e66879
teinhonglo <teinhonglo@gmail.com>
2019-03-25 22:50:38 +0800

    bit operation is better than long

--- problem/476-Number-Complement.cpp
+++ problem/476-Number-Complement.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int findComplement(int num) {
-        // convert int to complement bin
+        int k = 0;
+        while(k < num){
+            // shift mask
+            k = (k << 1) + 1;
+            //cout << bitset<8>(k) << endl;
+        }
+        // XOR operation
+        return k^num;
+	/*    
+	// convert int to complement bin
         long int_c = 0;
         long digit = 1;
         while(num){
@@ -12,5 +21,6 @@ public:
             digit *= 2;
         }
         return int_c;
+	*/
     }
 };
