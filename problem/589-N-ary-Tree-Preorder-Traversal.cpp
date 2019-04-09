ff570734254e49534448f05f36845a0b8e8c4d75
Tien-Hong Lo <teinhonglo@gmail.com>
2019-04-10 07:45:20 +0800

    adding iterative solution

--- problem/589-N-ary-Tree-Preorder-Traversal.cpp
+++ problem/589-N-ary-Tree-Preorder-Traversal.cpp
@@ -18,10 +18,28 @@ public:
     vector<int> po_vec;
     vector<int> preorder(Node* root){
         if(root == NULL) return po_vec;
+        /* recursive
         po_vec.push_back(root->val);
         
         for(int i = 0; i < root->children.size(); i++){
             preorder(root->children[i]);
+        }
+        return po_vec;
+        */
+        // iterative
+        vector<Node*> rec_vec;
+        Node* cn = root;
+        while(cn != NULL){
+            po_vec.push_back(cn->val);
+            for(int i = cn->children.size() - 1; i >= 0; i--){
+                rec_vec.push_back(cn->children[i]);
+            }
+            if(!rec_vec.empty()){
+                cn = rec_vec.back();
+                rec_vec.pop_back();
+            }else{
+                cn = NULL;
+            }
         }
         return po_vec;
     }
